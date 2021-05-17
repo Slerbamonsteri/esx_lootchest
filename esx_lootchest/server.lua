50364555fe66176d26d67cb1bd82fad04480b7ec
Slerbamonsteri <75551384+Slerbamonsteri@users.noreply.github.com>
2021-05-17 04:36:32 +0300

    Added support for cash-drops and some cleaning done
    Cleaned and simplified the code by alot, thanks to Weleho#7301

--- esx_lootchest/server.lua
+++ esx_lootchest/server.lua
@@ -2,27 +2,25 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-ESX.RegisterUsableItem("lootchest", function(source, itemName, howmany, weaponName, ammo)
-	local xPlayer = ESX.GetPlayerFromId(source)
-	local item = Config.LootChestitems[math.random(1, #Config.LootChestitems)]
-	local wep = Config.LootChestWeapons[math.random(1, #Config.LootChestWeapons)]
-	local chance = math.random(1, 100)
-	if Config.allowweapons then
-		if chance > Config.weaponchance then
-			xPlayer.removeInventoryItem('lootchest', 1)
-			TriggerClientEvent('slerba_lootchest:open', source)
-			Citizen.Wait(13400)
-			xPlayer.addInventoryItem(item.itemName, item.howmany)
-		else
-			xPlayer.removeInventoryItem('lootchest', 1)
-			TriggerClientEvent('slerba_lootchest:open', source)
-			Citizen.Wait(13400)
-			xPlayer.addWeapon(wep.weaponName, wep.ammo)
-		end
-	else
-		xPlayer.removeInventoryItem('lootchest', 1)
-		TriggerClientEvent('slerba_lootchest:open', source)
-		Citizen.Wait(13400)
-		xPlayer.addInventoryItem(item.itemName, item.howmany)
-	end
+ESX.RegisterUsableItem("lootchest", function(source)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+
+    xPlayer.removeInventoryItem('lootchest', 1)
+    TriggerClientEvent('slerba_lootchest:open', _source)
+    Citizen.Wait(13400)
+
+    local RandomBracket = math.random(1, #Config.LootChestitems)
+
+    for k,v in pairs(Config.LootChestitems[RandomBracket]) do 
+        if v.type == 'item' then
+            xPlayer.addInventoryItem(v.itemName, v.howmany)
+        elseif v.type == 'weapon' then
+            for i=1, v.howmany, 1 do
+                xPlayer.addWeapon(v.itemName, v.ammo)
+            end
+		elseif v.type == 'money' then
+            xPlayer.addMoney(v.howmany)
+        end
+    end
 end)
