5efbc5998b5b5c643e68230061f34a2ed116d463
Slerbamonsteri <75551384+Slerbamonsteri@users.noreply.github.com>
2021-05-17 04:37:15 +0300

    Added example brackets with money support

--- esx_lootchest/config.lua
+++ esx_lootchest/config.lua
@@ -1,17 +1,22 @@
 Config = {}
 
-Config.allowweapons = true --set True, if you want players to have a chance to get weapon from lootchest
-Config.weaponchance = 33 --Percentage for getting weapon from lootchest
+Config.LootChestitems = {
+ --Here are some examples using Items, Weapons and Money ( cash )
+    [1] = { 
+        { itemName = 'bread', howmany = 1, type = 'item'},
+		{ itemName = 'weapon_pistol', howmany = 1, ammo = 50, type = 'weapon'},
+		{ howmany = 69, type = 'money'},
+    },
+    
+    [2] = { 
+        { itemName = 'water', howmany = 1, type = 'item'},
+		{ itemName = 'weapon_pistol_mk2', howmany = 1, ammo = 50, type = 'weapon'},
+		{ howmany = 1337, type = 'money'},
+    },
 
-Config.LootChestitems = { --You can add as many brackets as you wish, just remember to index them correctly; [1], [2] and so on.
-	[1] = {itemName = 'rolex', howmany = 1},
-	[2] = {itemName = 'goldwatch', howmany = 1},
-	[3] = {itemName = 'bulletproof', howmany = 10},
-	[4] = {itemName = 'rifle', howmany = 1},
-}
-
-Config.LootChestWeapons = {
-	[1] = {weaponName = 'weapon_pistol50', ammo = 30},
-	[2] = {weaponName = 'weapon_pistol', ammo = 30},
-	[3] = {weaponName = 'weapon_vintagepistol', ammo = 30},
+    [3] = {
+        { itemName = 'water', howmany = 1, type = 'item'},
+		{ itemName = 'weapon_pistol50', howmany = 1, ammo = 50, type = 'weapon'},
+		{ howmany = 420, type = 'money'},
+    }
 }
